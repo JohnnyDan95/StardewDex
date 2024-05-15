2fe8b97a602282da3e3cc225371d057e7c3f5818
Dan GitGud <danrlei.sr@gmail.com>
2024-05-15 01:05:57 -0300

    Hello World!

---
+++ StardewDex/StardewDex.java
@@ -0,0 +1,11 @@
+package StardewDex;
+
+public class StardewDex {
+    public static void main (String [] args){
+        int sprinkler1 = 2;
+        int sprinkler2 = 8;
+        int sprinkler3 = 24;
+
+        
+    }
+}
